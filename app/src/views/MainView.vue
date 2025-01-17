76109b3a24c7d2a5582c1eddd4f0c7a2cdfdda58
cchuuu <chuyuanz@nycstudents.net>
2025-01-17 12:31:49 -0500

    a

---
+++ app/src/views/MainView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <main></main>
+  </div>
+</template>
+
+<script setup>
+import NormalFile from '.../src/component/NormalFile.vue'
+const main = NormalFile
+</script>
+
+<style scoped></style>
