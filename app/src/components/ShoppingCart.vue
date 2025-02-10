08845649655786e5cd9cb8a439ed7761a1cd2244
cchuuu <chuyuanz@nycstudents.net>
2025-02-10 12:32:20 -0500

    a

--- app/src/components/ShoppingCart.vue
+++ app/src/components/ShoppingCart.vue
@@ -4,16 +4,32 @@
       class="shoppingcart fixed top-0 right-0 w-72 h-full bg-gray-100 shadow-lg p-5 overflow-y-auto"
     >
       <h1 class="text-center text-xl font-bold">Shopping Cart</h1>
-      <li v-for="item in cartItems" :key="item.id">{{ item.name }} - \${{ item.price }}</li>
+      <ul>
+        <li v-for="item in cartItems" :key="item.id" class="flex justify-between">
+          <h3>{{ item.name }} ({{ item.quantity }})</h3>
+          <h3>\${{ item.price * item.quantity }}</h3>
+          <button @click="removeItem(item.id)" class="text-red-500">Remove</button>
+        </li>
+      </ul>
+      <div class="mt-4 font-semibold">Total: \${{ totalPrice }}</div>
     </div>
   </div>
 </template>
 
 <script setup>
-const cartItems = ref([])
+import { reactive } from 'vue'
+const cartItems = reactive([])
 defineProps({
   cartItems: Array,
 })
+
+function removeItem() {
+  const item = props.cartItems.find(item => item.id === id)
+  if (item){
+    if (item.quantity)
+  }
+  }
+}
 </script>
 
 <style scoped></style>
